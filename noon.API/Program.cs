4083c6ac1376bc8473aab7f66542b95bfd7e4868
d3bes <abdo.mohamed6319@gmail.com>
2023-07-14 19:56:12 +0300

    Add PostgreSql Context layer

--- noon.API/Program.cs
+++ noon.API/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using noon.Context.Context;
 using noon.Domain.Models.Identity;
+using Microsoft.Extensions.Configuration;
+using noon.PostgreSqlContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,11 +13,54 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<noonContext>(op =>
+
+////
+///s
+
+string dbProvider = builder.Configuration.GetSection("dbProvider").Value;
+
+if(dbProvider == "postgresql")
 {
-    op.UseSqlServer(builder.Configuration.GetConnectionString("Cs"));
-    //op.UseNpgsql(builder.Configuration.GetConnectionString("Cs"));
-});
+    builder.Services.AddDbContext<noonPostgrsContext>(op =>
+    {
+        op.UseNpgsql(builder.Configuration.GetConnectionString("postgresql"));
+    });
+
+
+    builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+    {
+        // Default Password settings.
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequiredLength = 6;
+        options.Password.RequiredUniqueChars = 0;
+    }).AddEntityFrameworkStores<noonPostgrsContext>()
+                .AddTokenProvider<DataProtectorTokenProvider<AppUser>>(TokenOptions.DefaultProvider);
+}
+else
+{
+    builder.Services.AddDbContext<noonContext>(op =>
+    {
+        op.UseSqlServer(builder.Configuration.GetConnectionString("Cs"));
+    });
+
+
+    builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+    {
+        // Default Password settings.
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequiredLength = 6;
+        options.Password.RequiredUniqueChars = 0;
+    }).AddEntityFrameworkStores<noonContext>()
+                .AddTokenProvider<DataProtectorTokenProvider<AppUser>>(TokenOptions.DefaultProvider);
+}
+
+
 builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
 {
     // Default Password settings.
---
+++ noon.PostgreSqlContext/Context/noonContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using noon.Domain.Models.Order;
+using noon.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using noon.Domain.Models.Identity;
+
+namespace noon.PostgreSqlContext
+{
+    public class noonPostgrsContext : IdentityDbContext<AppUser>
+    {
+        public noonPostgrsContext(DbContextOptions<noonPostgrsContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategorys { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
+        public DbSet<UserReview> CustomerReviews { get; set; }
+
+    }
+}
