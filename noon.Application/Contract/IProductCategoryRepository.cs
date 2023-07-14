900ebc83843ad3f97c4f713997231ed6f43bd1a8
n_k00001 <nooorykhalef@gmail.com>
2023-07-15 00:07:55 +0300

    add catogry repo

---
+++ noon.Application/Contract/IProductCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noon.Application.Contract
+{
+    public interface IProductCategoryRepository : IRepository<ProductCategory, int>
+    {
+        Task<IEnumerable<ProductCategory>> FilterByAsync(string filter, int id);
+    }
+}
