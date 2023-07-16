519a92bed46bfc41c4e363b89ca4ae019dca83fa
d3bes <abdo.mohamed6319@gmail.com>
2023-07-16 20:39:17 +0300

    user controller init

--- noon.Context/Context/noonContext.cs
+++ noon.Context/Context/noonContext.cs
@@ -8,25 +8,31 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using noon.Domain.Models.Identity;
+using noon.Context.Contracts;
 
 namespace noon.Context.Context
 {
-    public class noonContext : IdentityDbContext<AppUser>
+    public class noonContext : BaseContext  //: IdentityDbContext<AppUser>
     {
-        public noonContext(DbContextOptions<noonContext> options) : base(options)
+
+         public noonContext(DbContextOptions<BaseContext> options) : base(options)
         {
-
         }
+        
+        // public noonContext(DbContextOptions<noonContext> options) : base(options)
+        // {
 
-        public DbSet<Product> Products { get; set; }
-        public DbSet<ProductCategory> ProductCategorys { get; set; }
-        public DbSet<ProductBrand> ProductBrands { get; set; }
-        public DbSet<Image> Images { get; set; }
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<OrderItem> OrderItems { get; set; }
-        public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
-        public DbSet<UserReview> CustomerReviews { get; set; }
-        public DbSet<UserPaymentMethod> UserPaymentMethods { get; set; }
+        // }
 
+        // public DbSet<Product> Products { get; set; }
+        // public DbSet<ProductCategory> ProductCategorys { get; set; }
+        // public DbSet<ProductBrand> ProductBrands { get; set; }
+        // public DbSet<Image> Images { get; set; }
+        // public DbSet<Order> Orders { get; set; }
+        // public DbSet<OrderItem> OrderItems { get; set; }
+        // public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
+        // public DbSet<UserReview> CustomerReviews { get; set; }
+        // public DbSet<UserPaymentMethod> UserPaymentMethods { get; set; }
+       
     }
 }
---
+++ noon.Context/Contracts/BaseContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using noon.Domain.Models.Order;
+using noon.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using noon.Domain.Models.Identity;
+
+namespace noon.Context.Contracts
+{
+    public class BaseContext : IdentityDbContext<AppUser>
+    {
+           public BaseContext(DbContextOptions<BaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategorys { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
+        public DbSet<UserReview> CustomerReviews { get; set; }
+        public DbSet<UserPaymentMethod> UserPaymentMethods { get; set; }
+    }
+}
